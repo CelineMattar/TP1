5f950ea037f0d20e2c6305a213d4f0b393426980
mattarc <mattarc@educ.college-montreal.qc.ca>
2022-09-29 10:14:54 -0400

    code qui fonctionne

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-chaine = "yay (:"
-print(len(chaine))
-print(chaine[5])
-print(chaine[-2])
-print(chaine[:3])
-print("b" in chaine)
-print("(:" in chaine)
-print("b" not in chaine)
-#
-chaine_1 = "yay"
-chaine_2 = " (:"
-chaine_3 = chaine_1 + chaine_2
-print(chaine_3)
-print(len(chaine_3))
-print("nombre de caracteres: " + str(len(chaine)))
-#
-chaine_4 = chaine_1*3
-print(chaine_4)
+count_word = input("Dites quelqueschose")
+print("Nombre de caracteres:" + str(len(count_word)))
